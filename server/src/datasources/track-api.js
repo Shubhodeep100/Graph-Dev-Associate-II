6238b796a32c99c0792a41880ce2899304ad51f6
Shubhodeep <banerjeeshubho98@gmail.com>
2024-04-07 18:55:10 +0530

    APollo DataSource Implementation

---
+++ server/src/datasources/track-api.js
@@ -0,0 +1,10 @@
+const { RESTDataSource } = require("@apollo/datasource-rest");
+class TrackAPI extends RESTDataSource {
+  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+
+  getTracksForHome() {
+    return this.get("tracks");
+  }
+}
+
+module.exports = TrackAPI;
